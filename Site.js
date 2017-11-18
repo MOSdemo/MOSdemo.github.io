443b36d55121be1e512db7179e524f8db5ed8b40
ansulez <ansulez@gmail.com>
2017-11-17 22:21:39 -0500

    Added Animate.css.

--- Site.js
+++ Site.js
@@ -2,4 +2,14 @@
     $('.thumbnail').hover(function () {
         $('.slider', this).slideToggle('fast');
     });
+    //$('.thumbnail').hover(function () {
+    //    $('.slider', this).animate({
+    //        opacity: 0.75,
+            
+    //        height: "toggle"
+    //    }, 750, function () {
+    //        //Animation complete.
+    //    });
+    //});
+
 });
