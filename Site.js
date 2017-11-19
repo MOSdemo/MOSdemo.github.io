01d131e032fa5b1df916da565e81914a2f423fee
ansulez <ansulez@gmail.com>
2017-11-19 18:12:29 -0500

    Re-did the gallery to incorporate a fade-in instead of sliders.

--- Site.js
+++ Site.js
@@ -1,15 +1,26 @@
 ﻿$(document).ready(function () {
-    $('.thumbnail').hover(function () {
-        $('.slider', this).slideToggle('fast');
-    });
+    //$('.thumbnail').hover(function () {
+    //    $('.slider', this).slideToggle('fast');
+    //});
     //$('.thumbnail').hover(function () {
     //    $('.slider', this).animate({
     //        opacity: 0.75,
-            
+
     //        height: "toggle"
     //    }, 750, function () {
     //        //Animation complete.
-    //    });
+    // function customThemes() {
     //});
 
+    var customTheme = {};
+    var numThemes = 200;
+
+    for (var i = 0; i < numThemes; i++) {
+        customTheme["pastel" + i] = {
+            background: getRandomPastel(),
+            foreground: "#4E4E54"
+        };
+    }
+    return customTheme;
 });
+
