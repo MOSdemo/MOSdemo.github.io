002c9df2b6dfbbea309c438663c35b00e57c395e
ansulez <ansulez@gmail.com>
2017-11-23 17:14:11 -0500

    Left carousel arrow icons as-is.

--- Site.js
+++ Site.js
@@ -12,20 +12,16 @@
         });
 }
 
-$(document).ready(function () {
-
-
-
+//$(document).ready(function () {
     //$('.thumbnail').hover(function () {
     //    $('.slider', this).slideToggle('fast');
     //});
     //$('.thumbnail').hover(function () {
     //    $('.slider', this).animate({
     //        opacity: 0.75,
-
     //        height: "toggle"
     //    }, 750, function () {
     //        //Animation complete.
     // function customThemes() {
     //});
-});
+//});
